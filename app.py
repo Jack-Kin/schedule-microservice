44b77deb6e2d715fbfac78b381534debc755cbd7
Jack Kin <jinzhejian@outlook.com>
2021-10-23 23:05:37 -0400

    change app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_cors import CORS
 import json
 import logging
 
-from application_services.imdb_artists_resource import IMDBArtistResource
-from application_services.imdb_students_resource import IMDBStudentsResource
-from application_services.UsersResource.user_service import UserResource
+from application_services.SchedulesResource.schedule_service import ScheduleResource
 from database_services.RDBService import RDBService as RDBService
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,22 +15,27 @@ CORS(app)
 
 
 @app.route('/')
-def hello_world():
-    return '<u>Hello World!</u>'
+def hello_schedule():
+    return '<u>Hello this is the schedule microservice!</u>'
 
 
-@app.route('/imdb/artists/<prefix>')
-def get_artists_by_prefix(prefix):
-    res = IMDBArtistResource.get_by_name_prefix(prefix)
-    rsp = Response(json.dumps(res), status=200, content_type="application/json")
-    return rsp
+@app.route('/schedules', methods = ['POST','GET'])
+def get_schemes():
+    if request.method == 'GET':
+        res = ScheduleResource.get_by_template(None)
+        rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
+        return rsp
+    elif request.method == 'POST':
+        generate_row = ScheduleResource.create_schedule(request.json)
+        return '<u>Successfully post new schemes!</u>'
 
 
-@app.route('/users')
-def get_users():
-    res = UserResource.get_by_template(None)
-    rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
-    return rsp
+@app.route('/schedules/<prefix>', methods = ['POST','GET','DELETE','UPDATE'])
+def order_id(prefix):
+    if request.method == 'GET':
+        res = ScheduleResource.get_by_schedule_id(prefix)
+        rsp = Response(json.dumps(res), status=200, content_type="application/json")
+        return rsp
 
 
 @app.route('/<db_schema>/<table_name>/<column_name>/<prefix>')
@@ -42,13 +45,6 @@ def get_by_prefix(db_schema, table_name, column_name, prefix):
     return rsp
 
 
-# add
-@app.route('/students/<name>')
-def get_by_name(name):
-    res = IMDBStudentsResource.get_by_name(name)
-    rsp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
-    return rsp
-
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5003)
